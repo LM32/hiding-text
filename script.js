681103ed15c27b51d023a8aa9aeefad31e9ac1be
LM32 <LM32@users.noreply.github.com>
2022-01-20 14:40:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function hideQuote() {
+    let hiding = document.getElementById("quote");
+    if(hiding.style.display==="none") {
+        hiding.style.display = "block";
+    } else {
+        hiding.style.display = "none";
+    }
+}
+
+// https://www.youtube.com/watch?v=OLyudRnhYN8 Start: 6:07 Stop: 13:49
